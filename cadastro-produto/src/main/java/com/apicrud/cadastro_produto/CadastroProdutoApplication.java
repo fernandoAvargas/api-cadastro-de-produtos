967ff13f6a1a1a2f4ce9ebe9a6a578f8d622e1e6
fernandoAvargas <fernan.assis@hotmail.com>
2025-11-16 23:08:32 -0300

    Initial

---
+++ cadastro-produto/src/main/java/com/apicrud/cadastro_produto/CadastroProdutoApplication.java
@@ -0,0 +1,12 @@
+package com.apicrud.cadastro_produto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroProdutoApplication {
+	public static void main(String[] args) {
+        System.out.println("Bom dia Fernando!...");
+		SpringApplication.run(CadastroProdutoApplication.class, args);
+	}
+}
