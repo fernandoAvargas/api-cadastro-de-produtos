3a1fecb17a75c7e8c4b933963407f4befb666fe6
fernandoAvargas <fernan.assis@hotmail.com>
2025-11-17 00:16:11 -0300

    Infrastructure ready

--- cadastro-produto/src/main/java/com/apicrud/cadastro_produto/infrastructure/repositories/produtoRepository.java
+++ cadastro-produto/src/main/java/com/apicrud/cadastro_produto/infrastructure/repositories/produtoRepository.java
@@ -2,4 +2,5 @@ package com.apicrud.cadastro_produto.infrastructure.repositories;
 import com.apicrud.cadastro_produto.infrastructure.entities.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@SuppressWarnings("unused")
 public interface produtoRepository extends JpaRepository<Produto,Integer> {}
